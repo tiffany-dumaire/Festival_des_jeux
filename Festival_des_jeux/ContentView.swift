360802c33a8fa896b9b7b841bd8e423c6e6039c0
Tiffany Dumaire <tiffany.du89@live.fr>
2021-03-21 16:26:07 +0100

    mainView fini / login view fini

--- Festival_des_jeux/ContentView.swift
+++ Festival_des_jeux/ContentView.swift
@@ -9,7 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("test")
+        ZStack{
+            VStack{
+                HStack{
+                    Image("logo")
+                    .resizable()
+                        .frame(minWidth: 110, idealWidth: 110, maxWidth: 110, minHeight: 90, idealHeight: 90, maxHeight: 90, alignment: .topLeading)
+                    Spacer()
+                }
+                Spacer()
+                VStack{
+                    Text("Je suis un organisateur :")
+                    NavigationLink(
+                        destination: LoginView(),
+                        label: {
+                            Text("Se connecter")
+                        })
+                    .padding()
+                    .background(Color.green)
+                    .cornerRadius(30)
+                    .foregroundColor(.white)
+                }
+                Spacer()
+                VStack{
+                    Text("Je suis un visiteur :")
+                    NavigationLink(
+                        destination: LoginView(),
+                        label: {
+                            Text("Voir les informations\ndu prochain festival")
+                                .multilineTextAlignment(.center)
+                        })
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(30)
+                    .foregroundColor(.white)
+                }
+                Spacer()
+            }
+        }.background(
+            Image("backgroundStartView")
+                .resizable()
+                .scaledToFill()
+                .clipped()
+        )
+        .edgesIgnoringSafeArea(.all)
     }
 }
 

2fd88e9df4323f442ac7460a03bc6184c22aa12e
Tiffany Dumaire <tiffany.du89@live.fr>
2021-03-21 12:26:46 +0100

    Initial Commit

---
+++ Festival_des_jeux/Festival_des_jeuxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Festival_des_jeuxApp.swift
+//  Festival_des_jeux
+//
+//  Created by user188898 on 3/21/21.
+//
+
+import SwiftUI
+
+@main
+struct Festival_des_jeuxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
